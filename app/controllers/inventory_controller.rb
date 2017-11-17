30386946e2c24747fd816a73da21d5aa36ad271c
Dany Samuel <danysamuel@gmail.com>
2017-11-17 19:05:32 +1030

    Integrated Upload...

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -17,8 +17,12 @@ class InventoryController < ApplicationController
         xlsx = Roo::Spreadsheet.open('test.csv')
         
         
+        arraycnt = 0
+        produtsids = []
+
+        # <Update records from CSV>
+
         xlsx.sheet(0).each(productid: 'ProductID', category: 'Category',product_name: 'Product Name', price: 'Price') do |line|
-           
             if (line[:productid] != "ProductID")
                 @product=Product.new
                 @product.productid=line[:productid]
@@ -31,13 +35,30 @@ class InventoryController < ApplicationController
                 else
                     Product.update(@product.productid,line)
                 end
+                
+                produtsids[arraycnt]=@product.productid
 
+                arraycnt = arraycnt +1
+                
             end
-
-           
-            
             
         end
+
+        # </Update records from CSV>
+        
+        
+        # <Delete records which are removed from CSV>
+        @products=Product.all
+
+        @products.each do |product|
+           
+            if !product.productid.in?(produtsids)
+                product.destroy
+            end
+        end
+
+        # </Delete records which are removed from CSV>
+        
         redirect_to inventory_index_path
 
     end
